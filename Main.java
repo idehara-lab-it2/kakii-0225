e4d12404b89a66678e753b3f816cfc93e993e7fd
kakii0225 <katagiriyuiti@icloud.com>
2020-07-20 01:29:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main {
+  <city title="名護" id="471020" source="http://weather.livedoor.com/forecast/rss/area/471020.xml"/>
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
